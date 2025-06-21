2666614e6808921769ace41d915575182c031eca
phu <102038067+grillitpls@users.noreply.github.com>
2025-06-22 01:26:56 +0700

    done

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const title = document.querySelector('input[name="title"]')
 const author = document.querySelector('input[name="author"]')
 const pages = document.querySelector('input[name="pages"]')
 const read = document.querySelector('input[name="read"]')
+
 readStatus = () => read.checked ? 'read' : 'not read yet'
 
 console.log(form)
@@ -12,7 +13,7 @@ console.log(title)
 console.log(readStatus)
 
 
-const myLibrary = [];
+let myLibrary = [];
 let bookHTML = ``
 
 console.log(myLibrary.length)
@@ -49,14 +50,33 @@ addBtn.addEventListener('click', (e) => {
     form.reset()
 })
 
+function toggleRead() {
+    if(document.querySelector('.read-toggle').textContent === 'read') {
+        document.querySelector('.read-toggle').textContent = 'not read yet'
+    }else {
+        document.querySelector('.read-toggle').textContent  = 'read'
+    }
+}
+
+function Delete(element, event) {
+    element.parentNode.remove()
+    let newLibrary = myLibrary.filter(book => {
+        console.log(element)
+        return book.id != element.dataset.id
+    })
+    myLibrary = newLibrary
+    console.log(myLibrary)
+}
+
 function displayBook() {
     if (myLibrary.length > 0) {
         bookHTML = `
             <div class="book">
+                <button class="delete" onclick="Delete(this, event)" data-id="${myLibrary[myLibrary.length - 1].id}">delete</button>
                 <p class='text-wrap'>${myLibrary[myLibrary.length - 1].title}</p>
                 <p class='text-wrap'>${myLibrary[myLibrary.length - 1].author}</p>
                 <p class='text-wrap'>${myLibrary[myLibrary.length - 1].pages}</p>
-                <p class='text-wrap'>${myLibrary[myLibrary.length - 1].read}</p>
+                <button class="read-toggle" onclick="toggleRead()">${myLibrary[myLibrary.length - 1].read}</button>
             </div>
         `
         
