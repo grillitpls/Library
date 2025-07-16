20968aa5d75b900c881cdde0fbff7742d940f27b
phu <102038067+grillitpls@users.noreply.github.com>
2025-07-17 01:39:39 +0700

    letsgo

--- script.js
+++ script.js
@@ -6,55 +6,67 @@ const author = document.querySelector('input[name="author"]')
 const pages = document.querySelector('input[name="pages"]')
 const read = document.querySelector('input[name="read"]')
 
-readStatus = () => read.checked ? 'read' : 'not read yet'
+let myLibrary = []
+let readStatus = () => read.checked ? 'read' : 'not read yet'
 
-console.log(form)
-console.log(title)
-console.log(readStatus)
+class Book {
+    bookHTML = ``
 
-
-let myLibrary = [];
-let bookHTML = ``
-
-console.log(myLibrary.length)
-
-function Book(title, author, pages, read) {
-    if  (!new.target) {
-        throw Error("You must use the 'new operator to call the constructor");
+    constructor(title, author, pages, read) {
+        if (!new.target) {
+            throw Error("You must use the new operator to call the constructor")
+        }
+        this.id = crypto.randomUUID()
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read()
     }
-    
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read
-}
 
-function addBookToLibrary(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read)
-    
-    myLibrary.push(newBook)
+    displayBook() {
+        const dataID = myLibrary[myLibrary.length - 1].id
 
-    console.log(myLibrary, myLibrary.length)
+        if (myLibrary.length > 0) {
+            this.bookHTML = `
+                <div class="book">
+                    <button class="delete" onclick="Delete(this, event)" data-id="${myLibrary[myLibrary.length - 1].id}">delete</button>
+                    <p class="text-wrap">${myLibrary[myLibrary.length - 1].title}</p>
+                    <p class="text-wrap">${myLibrary[myLibrary.length - 1].author}</p>
+                    <p class="text-wrap">${myLibrary[myLibrary.length - 1].pages}</p>
+                    <button class="read-toggle" data-id="${myLibrary[myLibrary.length - 1].id}" onclick="toggleRead(this)">${myLibrary[myLibrary.length - 1].read}</button>
+                </div>
+            `
+        }
+        container.innerHTML += this.bookHTML
+    }
 }
 
 addBtn.addEventListener('click', (e) => {
     e.preventDefault()
 
     if (title.value !== '' && author.value !== '') {
-        addBookToLibrary(title.value, author.value, pages.value, readStatus())
+        const newBook = new Book(title.value, author.value, pages.value, readStatus)
+        
+        myLibrary.push(newBook)
+        
+        console.log(myLibrary)
     
-        displayBook()
+        newBook.displayBook()       
     }
 
     form.reset()
-})
+}) 
 
-function toggleRead() {
-    if(document.querySelector('.read-toggle').textContent === 'read') {
-        document.querySelector('.read-toggle').textContent = 'not read yet'
+function toggleRead(element) {
+    console.log(Array.from(document.querySelectorAll('.read-toggle')))
+    const found = Array.from(document.querySelectorAll('.read-toggle')).find((button) => button.dataset.id === element.dataset.id)
+
+    console.log(found)
+
+    if(found.textContent === 'read') {
+        found.textContent = 'not read yet'
     }else {
-        document.querySelector('.read-toggle').textContent  = 'read'
+        found.textContent = 'read'
     }
 }
 
@@ -65,21 +77,4 @@ function Delete(element, event) {
         return book.id != element.dataset.id
     })
     myLibrary = newLibrary
-    console.log(myLibrary)
-}
-
-function displayBook() {
-    if (myLibrary.length > 0) {
-        bookHTML = `
-            <div class="book">
-                <button class="delete" onclick="Delete(this, event)" data-id="${myLibrary[myLibrary.length - 1].id}">delete</button>
-                <p class='text-wrap'>${myLibrary[myLibrary.length - 1].title}</p>
-                <p class='text-wrap'>${myLibrary[myLibrary.length - 1].author}</p>
-                <p class='text-wrap'>${myLibrary[myLibrary.length - 1].pages}</p>
-                <button class="read-toggle" onclick="toggleRead()">${myLibrary[myLibrary.length - 1].read}</button>
-            </div>
-        `
-        
-        container.innerHTML += bookHTML
-    }
 }
